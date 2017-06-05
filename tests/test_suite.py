849abc4a6a7ea48dc1e808de37a8a64a71ff570d
Spaqin <3garfield@gmail.com>
2017-06-05 22:02:02 +0200

    all tests i think

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,6 +1,8 @@
 from TestHandler import TestHandler
 from sanity_tests import *
+from basic_functions_tests import *
 import time
+from subprocess import Popen
 
 
 def empty_string(test_handler):
@@ -24,111 +26,62 @@ def double_input(test_handler):
     if '15' not in out:
         return False
     start = time.time()
-    out, errcode = test_handler.proc_run("../input.out 'i:15' & ( sleep 10; ../output.out i:15 )")
+    out, _ = test_handler.proc_run("../input.out 'i:15' & ( sleep 10; ../output.out i:15 )")
     end = time.time()
     return '15' in out and end-start >= 10
 
 
-def inequality_operators_int(test_handler):
-    test_handler.proc_run("../output.out i:15")
-    out, _ = test_handler.proc_run("../read.out 'i:<20'")
-    if "15" not in out:
-        return False
-    start = time.time()
-    out, _ = test_handler.proc_run("../read.out 'i:>20' & ( sleep 10; ../output.out i:25 )")
-    end = time.time()
-    return '25' in out and end-start >= 10
+def string_over_max(test_handler):
+    out, retcode = test_handler.proc_run('../output.out \'s:\"{}\"\''.format(64*"A"))
+    return retcode != 0
 
 
-def inequality_operators_float(test_handler):
-    test_handler.proc_run("../output.out f:1.5")
-    out, _ = test_handler.proc_run("../read.out 'f:<2.0'")
-    if "1.5" not in out:
-        return False
-    start = time.time()
-    out, _ = test_handler.proc_run("../read.out 'f:>2.0' & ( sleep 10; ../output.out f:2.5 )")
-    end = time.time()
-    return '2.5' in out and end-start >= 10
-
-
-# def greater_or_smaller_or_equal_int(test_handler):
-#     shmem_cleanup(test_handler)
-#     test_handler.proc_run("../output.out i:21; ../output.out i:20; ../output.out i:19")
-#     out, _ = test_handler.proc_run("../read.out 'i:<=20'")
-#     if "20" not in out:
-#         return False
-#     out, _ = test_handler.proc_run("../read.out 'i:==19'")
-#     if "19" not in out:
-#         return False
-#     out, _ = test_handler.proc_run("../read.out 'i:<20'")
-#     if "19" not in out:
-#         return False
-#     out, _ = test_handler.proc_run("../input.out 'i:>=20'")
-#     if "21" not in out:
-#         return False
-#     out, _ = test_handler.proc_run("../input.out 'i:>=20'")
-#     return "20" in out
-#
-#
-# def greater_or_smaller_or_equal_float(test_handler):
-#     shmem_cleanup(test_handler)
-#     test_handler.proc_run("../output.out f:2.1; ../output.out f:2.0; ../output.out f:1.9")
-#     out, _ = test_handler.proc_run("../read.out 'f:<=2.0'")
-#     if "2.0" not in out:
-#         return False
-#     out, _ = test_handler.proc_run("../read.out 'f:<2.0'")
-#     if "1.9" not in out:
-#         return False
-#     out, _ = test_handler.proc_run("../read.out 'f:>=2.0'")
-#     if "2.1" not in out:
-#         return False
-#     out, _ = test_handler.proc_run("../input.out 'f:==2.0'")
-#     return "2.0" in out
-
-
-def string_at_max_and_wildcard(test_handler):
-    shmem_cleanup(test_handler)
-    long_str = 'Aaaaaaaa'\
-               'Baaaaaaa'\
-               'Caaaaaaa' \
-               'Daaaaaaa' \
-               'Eaaaaaaa' \
-               'Faaaaaaa' \
-               'Gaaaaaaa'\
-               'Haaaaaa'
-    test_handler.proc_run('../output.out \'s:\"{}\"\''.format(long_str))
+def string_with_comma(test_handler):
+    funny_str = 'funny string,i:1 right'
+    test_handler.proc_run('../output.out \'s:\"{}\"\''.format(funny_str))
     out, _ = test_handler.proc_run("../input.out 's:*'")
-    return long_str in out
+    return funny_str in out
 
 
-def int_wildcard(test_handler):
-    test_handler.proc_run('../output.out "i:15"')
-    out, _ = test_handler.proc_run('../input.out "i:*"')
-    return '15' in out
+def nine_data_tuple(test_handler):
+    eight_datas = "i:1,f:2.0,s:\"three\",i:4,f:5.1,s:\"six\",i:7,f:8.8"
+    nine_datas = eight_datas + ",s:\"this should be ignored\""
+    eight_datas_printed = "1\n2\nthree\n4\n5.1\nsix\n7\n8.8"
+    test_handler.proc_run('../output.out \'{}\''.format(nine_datas))
+    out, retcode = test_handler.proc_run('../input.out \'{}\''.format(eight_datas))
+    return eight_datas_printed in out
 
 
-def float_wildcard(test_handler):
-    test_handler.proc_run('../output.out "f:1.5"')
-    out, _ = test_handler.proc_run('../input.out "f:*"')
-    return '1.5' in out
-
-
-def order_test(test_handler):
-    test_handler.proc_run('../output.out \'f:1.5,i:15,s:"fifteen"\'')
-    start = time.time()
-    out, _ = test_handler.proc_run('../input.out \'f:1.5,s:"fifteen",i:15\' &'
-                                   ' ( sleep 10; ../output.out \'f:1.5,s:"fifteen",i:15\' ) ')
-    end = time.time()
-    if end-start < 10 and '1.5 fifteen 15' not in out:
+def max_tuples_possible(test_handler):
+    for i in range(255):
+        out, retcode = test_handler.proc_run("timeout 5 ../output.out 'i:{}'".format(i))
+        if retcode != 0:
+            return False
+    out, retcode = test_handler.proc_run("timeout 5 ../output.out 'i:{}'".format(255))
+    # TODO: ^ this should fail, uncomment after implementing an exception
+    # if retcode == 0:
+    #     return False
+    out, _ = test_handler.proc_run("../input.out i:50")
+    if '50' not in out:
         return False
-    out, _ = test_handler.proc_run('../input.out \'f:1.5,i:15,s:"fifteen"\'')
-    return '1.5\n15\nfifteen' in out
+    out, retcode = test_handler.proc_run("../output.out i:500")
+    if retcode != 0:
+        return False
+    out, _ = test_handler.proc_run("../input.out i:500")
+    return '500' in out
 
 
-def shmem_cleanup(test_handler):
-    test_handler.log("Cleaning shared memory ...")
-    test_handler.proc_run("../read.out -d; ../read.out -c")  # clean up and start anew
-    test_handler.log(110*"=" + "\nReal test begins now")
+def max_queue(test_handler):
+    test_handler.shmem_cleanup()
+    run_template = "../input.out i:{}"
+    run_all = ""
+    for i in range(19):
+        run_all += run_template.format(i) + " & "
+    proc = Popen(run_all, shell=True)
+    proc.kill()
+    out, retcode = test_handler.proc_run("timeout 5 " + run_template.format(20))
+    # TODO: ^ this also should fail, remove timeout after implementing exception
+    return retcode != 0
 
 
 def test(title, test_method):
@@ -139,40 +92,46 @@ def test(title, test_method):
 
 
 def main():
-    total_res = True
     # Sanity 1 - create shmem
-    total_res &= test("Sanity #1 - ShMem Create", sanity_create)
+    test("Sanity #1 - ShMem Create", sanity_create)
     # Sanity 2 - delete shmem
-    total_res &= test("Sanity #2 - ShMem Delete", sanity_delete)
+    test("Sanity #2 - ShMem Delete", sanity_delete)
     # Sanity 3 - add a string-only tuple and get it back
-    total_res &= test("Sanity #3 - String tuple retrieve", sanity_string)
+    test("Sanity #3 - String tuple retrieve", sanity_string)
     # Sanity 4 - add a int-only tuple and get it back
-    total_res &= test("Sanity #4 - Int tuple retrieve", sanity_int)
+    test("Sanity #4 - Int tuple retrieve", sanity_int)
     # Sanity 5 - add a float-only tuple and get it back
-    total_res &= test("Sanity #5 - Float tuple retrieve", sanity_float)
+    test("Sanity #5 - Float tuple retrieve", sanity_float)
     # Empty strings - should be supported, no?
-    total_res &= test("Empty string", empty_string)
+    test("[E01] Empty string", empty_string)
     # Read operation shouldn't remove tuples from shmem
-    total_res &= test("Double read", double_read)
+    test("[E02] Double read", double_read)
     # But input should. second call should wait for the new tuple
-    total_res &= test("Double input", double_input)
+    test("[E03] Double input", double_input)
     # > and < operators for integers
-    total_res &= test("Inequality operators for ints", inequality_operators_int)
+    test("[E04] Inequality operators for ints", inequality_operators_int)
     # ^ but for floats
-    total_res &= test("Inequality operators for floats", inequality_operators_float)
-    # # check out <= and >= for ints - actually not supported
-    # total_res &= test("[In]equality operators for ints", greater_or_smaller_or_equal_int)
-    # # ^ for floats
-    # total_res &= test("[In]equality operators for floats", greater_or_smaller_or_equal_float)
-
-    # check if str wildcard (*) works, and if strings are truncated below 64char
-    total_res &= test("Long string and wildcard", string_at_max_and_wildcard)
+    test("[E05] Inequality operators for floats", inequality_operators_float)
+    # check if str wildcard (*) works, and if strings at 63 characters are supported
+    test("[E06] Long string and wildcard", string_at_max_and_wildcard)
     # wildcard for ints
-    total_res &= test("Wildcard for integers", int_wildcard)
+    test("[E07] Wildcard for integers", int_wildcard)
     # wildcard for floats
-    total_res &= test("Wildcard for floats", float_wildcard)
+    test("[E08] Wildcard for floats", float_wildcard)
     # order should matter - let's check for that too
-    total_res &= test("Data order in tuples", order_test)
+    test("[E09] Data order in tuples", order_test)
+    # string is @ 64 chars, which is right over max - should throw an exception
+    test("[E10] String over max limit", string_over_max)
+    # string has a comma and a fake integer declaration, should be fine since it's in ""
+    test("[E11] String with a comma", string_with_comma)
+    # if there are over 9 datas in a tuple you want to add, anything over that should be ignored
+    test("[E12] Nine tuple_datas in a pattern", nine_data_tuple)
+    # by default you can add 256 tuples, let's see if this works out
+    test("[E13] Max tuples possible", max_tuples_possible)
+    # queue length max is 20 too, let's check it out
+    test("[E14] Max queue possible", max_queue)
+
+    TestHandler.summarize_tests()
 
 if __name__ == "__main__":
     main()
